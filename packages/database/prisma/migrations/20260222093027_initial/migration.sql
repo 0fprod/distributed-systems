d0dbb6b6ff1fc8889329b7351936d6d8eb2a41b2
0fprod <franp.dev@protonmail.com>
2026-02-23 10:20:58 +0000

    refactor: add mysql+adminer

--- packages/database/prisma/migrations/20260222093027_initial/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Invoice" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "amount" REAL NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'pending',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
